a9b844e4780071021866843ba97aa1c72693bd55
Simone <simopedro94@hotmail.it>
2024-01-23 21:25:26 +0100

    Metodo completato funzionante, primi due test completati e funzionanti

---
+++ appello_2019_02_01/appello_2019_02_01/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace appello_2019_02_01
+{
+    public static class Class1
+    {
+        public static void Main()
+        {
+            var seq = new [] { 1, -6, 7, 4, 6, 2, -3, 4, -4 };
+            foreach (var item in seq.EvenOddSwap())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<T> EvenOddSwap<T>(this IEnumerable<T> seq)
+        {
+            if (seq == null)
+                throw new ArgumentNullException();
+            while (seq.Any())
+            {
+                var aux = seq.Take(2).ToArray();
+                seq = seq.Skip(2);
+                if (seq.Count() < 1)
+                    yield return aux[0];
+                else
+                {
+                    yield return aux[1];
+                    yield return aux[0];
+                }
+            }
+        }
+    }
+}
---
+++ appello_2019_02_01/appello_2019_02_01_test/Class1.cs
@@ -0,0 +1,33 @@
+﻿using appello_2019_02_01;
+using NUnit.Framework;
+
+namespace appello_2019_02_01_test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test1()
+        {
+            List<int> aux = null;
+            Assert.Throws<ArgumentNullException>(() => aux.EvenOddSwap().ToList());
+        }
+
+        [Test]
+        public void test2()
+        {
+            var list = new List<string>()
+            {
+                "Donald duck","Lowie","Dewey","Hewey","Scrooge mc Duck"
+            };
+
+            var expected = new List<string>()
+            {
+                "Lowie","Donald duck","Hewey","Dewey","Scrooge mc Duck"
+            };
+
+            Assert.That(list.EvenOddSwap(),Is.EqualTo(expected));
+        }
+
+    }
+}
