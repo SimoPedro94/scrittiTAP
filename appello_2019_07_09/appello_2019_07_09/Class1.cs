9c6e6973f14984b5f63caa1f4705026f5749ff5e
Simone <simopedro94@hotmail.it>
2024-01-23 17:32:55 +0100

    Extension Method completato, test 1 e 2 completati.

---
+++ appello_2019_07_09/appello_2019_07_09/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace appello_2019_07_09
+{
+    public static class Class1
+    {
+        public static void Main()
+        {
+            string word = "cane";
+            foreach (var elem in word.MexicanWave().Take(10).ToList())
+            {
+                Console.WriteLine(elem);
+            }
+        }
+
+        public static IEnumerable<string> MexicanWave(this string word)
+        {
+            if (word == null)
+                throw new ArgumentNullException();
+            if (word.Length < 2)
+                throw new ArgumentException();
+            while (true)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    string res = "";
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (!char.IsLetter(word[i]))
+                            throw new ArgumentException();
+                        if (j == i)
+                            res += char.ToUpper(word[i]);
+                        else res += char.ToLower(word[i]);
+                    }
+                    yield return res;
+                }
+            }
+        }
+    }
+}
---
+++ appello_2019_07_09/appello_2019_07_09_test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using appello_2019_07_09;
+using NUnit.Framework;
+
+namespace appello_2019_07_09_test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            string word = "cane";
+            var expected = new List<string>() {"Cane", "cAne", "caNe", "canE", "Cane", "cAne", };
+            Assert.That(expected,Is.EqualTo(word.MexicanWave().Take(6).ToList()));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string word = "ga-to";
+            Assert.Throws<ArgumentException>(() => word.MexicanWave().ToList());
+        }
+    }
+}
