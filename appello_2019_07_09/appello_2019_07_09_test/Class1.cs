3300adc3f5a08c93e1f3fb4d8bdcfca716625ffe
Simone <simopedro94@hotmail.it>
2024-01-23 20:45:01 +0100

    Completato anche test3, funzionante

--- appello_2019_07_09/appello_2019_07_09_test/Class1.cs
+++ appello_2019_07_09/appello_2019_07_09_test/Class1.cs
@@ -13,12 +13,27 @@ namespace appello_2019_07_09_test
             var expected = new List<string>() {"Cane", "cAne", "caNe", "canE", "Cane", "cAne", };
             Assert.That(expected,Is.EqualTo(word.MexicanWave().Take(6).ToList()));
         }
-
+        
         [Test]
         public void Test2()
         {
             string word = "ga-to";
             Assert.Throws<ArgumentException>(() => word.MexicanWave().ToList());
+        }
+        
+        [TestCase(2, "pan")]
+        public void Test3(int howmany, string word)
+        {
+            var res = word.MexicanWave().Take(howmany*word.Length).ToList();
+            Assert.Multiple(() =>
+            {
+                foreach (var elem in res)
+                {
+                    Assert.That(elem.Length,Is.EqualTo(word.Length));
+                    Assert.That(elem.Where(s=>char.IsUpper(s)).Count(),Is.EqualTo(1));
+                    Assert.That(elem.Any(s=>!char.IsLetter(s)),Is.False);
+                }
+            });
         }
     }
 }
