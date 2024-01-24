06febd37f46b86bde4938337e5468e552dc37a3d
Simone <simopedro94@hotmail.it>
2024-01-24 11:20:45 +0100

    Completati sia metodo che tutti i test del 8/9/2019, funzionanti

---
+++ appello_2017_09_08/appello_2017_09_08/Class1.cs
@@ -0,0 +1,51 @@
+﻿namespace appello_2017_09_08
+{
+    public static class Class1
+    {
+        public static void Main(string[] args)
+        {
+            var seq = new string[] { "a 0", "b 1", "c 2", "d 3", "e 4", "f 5", "g 6" };
+            Console.Write(" First 2 elements on some prime position are: ");
+            foreach (var s in seq.TakePrime(2))
+                Console.Write("{0} ,", s);
+            Console.WriteLine();
+            Console.Write(" First 100 elements on some prime position are: ");
+            foreach (var s in seq.TakePrime(100))
+                Console.Write(" {0} ,", s);
+            Console.WriteLine();
+        }
+
+        public static IEnumerable<T> TakePrime<T>(this IEnumerable<T> s, int count)
+        {
+            if (s == null)
+                throw new ArgumentNullException();
+            if(count < 1)
+                throw new ArgumentOutOfRangeException();
+            var E_s = s.GetEnumerator();
+            int i = 0;
+            int aux = 0;
+            while(E_s.MoveNext()) 
+            {
+                if(i == count)
+                    yield break;
+                if (IsPrimo(aux))
+                {
+                    i++;
+                    yield return E_s.Current;
+                }
+                aux++;
+            }
+        }
+
+        private static bool IsPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ appello_2017_09_08/appello_2017_09_08_test/Class1.cs
@@ -0,0 +1,63 @@
+﻿using appello_2017_09_08;
+using NUnit.Framework;
+
+namespace appello_2017_09_08_test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var list = Enumerable.Range(0, 20);
+            var expected = new List<int>() {2,3,5,7,11,13};
+            Assert.That(expected,Is.EqualTo(list.TakePrime(6)));
+        }
+
+        [TestCase(2)]
+        [TestCase(3)]
+        public void Test2(int b)
+        {
+            var list = InfinitePow(b);
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.TakePrime(0).ToList());
+        }
+
+        [TestCase(7)]
+        [TestCase(10)]
+        public void Test3(int size)
+        {
+            var list = InfiniteSeq().TakePrime(size);
+            var res = list.GetEnumerator();
+            Assert.Multiple(() =>
+            {
+                Assert.That(list.Count(),Is.EqualTo(size));
+                res.MoveNext();
+                var aux = res.Current;
+                while (res.MoveNext())
+                {
+                    Assert.IsTrue(res.Current > aux);
+                }
+            });
+        }
+
+        private IEnumerable<int> InfinitePow(int n)
+        {
+            int i = n;
+            while (true)
+            {
+                yield return i;
+                i = n * i;
+            }
+        }
+
+        private IEnumerable<int> InfiniteSeq()
+        {
+            int i = 0;
+            while (true)
+            {
+                yield return i;
+                i++;
+            }
+        }
+    }
+}
