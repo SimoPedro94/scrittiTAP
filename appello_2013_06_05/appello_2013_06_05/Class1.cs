4f91141b0d76fff02fb625824588c1862269bd0e
Simone <simopedro94@hotmail.it>
2024-01-24 12:32:36 +0100

    Completati il metodo e due test, funzionanti

---
+++ appello_2013_06_05/appello_2013_06_05/Class1.cs
@@ -0,0 +1,54 @@
+﻿namespace appello_2013_06_05
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(-3, 10);
+            var pred = new List<Predicate<int>>()
+            {
+                i => i >= 0 && i % 2 == 0,
+                i => i >= 0 && i % 2 != 0,
+                i => i < -2
+            };
+            foreach (var partition in Classify(numbers, pred))
+            {
+                foreach (var item in partition)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static List<T>[] Classify<T>(IEnumerable<T> s, IEnumerable<Predicate<T>> p)
+        {
+            if(s == null || p == null)
+                throw new ArgumentNullException();
+
+            var res = new List<List<T>>();
+            var toRemove = new List<T>();
+
+            foreach (var pred in p)
+            {
+                var aux = new List<T>();
+
+                foreach (var elem in s)
+                {
+                    if (pred(elem))
+                    {
+                        if (!toRemove.Contains(elem))
+                        {
+                            aux.Add(elem);
+                            toRemove.Add(elem);
+                        }
+                    }
+                }
+                res.Add(aux);
+            }
+            if(toRemove.Count != s.Count())
+                res.Add(s.Except(toRemove).ToList());
+            return res.ToArray();
+        }
+    }
+}
---
+++ appello_2013_06_05/appello_2013_06_05_test/Class1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace appello_2013_06_05_test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var numbers = Enumerable.Range(-3, 10);
+            var pred = new List<Predicate<int>>()
+            {
+                i => i >= 0 && i % 2 == 0,
+                i => i >= 0 && i % 2 != 0,
+                i => i < -2
+            };
+            var expected = new List<int>[]
+            {
+                new List<int>{0,2,4,6},
+                new List<int>{1,3,5},
+                new List<int>{-3},
+                new List<int>{-2,-1}
+            };
+            Assert.That(expected,Is.EqualTo(appello_2013_06_05.Class1.Classify(numbers,pred)));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var numbers = Enumerable.Range(-3, 10);
+            List<Predicate<int>> pred = null;
+            List<int>[] expected = null;
+            Assert.Throws<ArgumentNullException>(() => appello_2013_06_05.Class1.Classify(numbers, pred).ToList());
+        }
+    }
+}
