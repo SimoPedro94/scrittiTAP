0b6f090d22a175982a77b29b81097efcc3444b24
Simone <simopedro94@hotmail.it>
2024-01-24 16:55:03 +0100

    Progetto di test per esercizio con ock

---
+++ test_mio_per_Mock/test_mio_per_Mock/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace test_mio_per_Mock
+{
+    public static class Class1
+    {
+        public interface IMyinterface
+        {
+            public int Argomento1 { get; }
+            public int Argomento2 { get; }
+        }
+
+        public class MyClass : IMyinterface
+        {
+           public int argomento1 { get; set; }
+           public int argomento2 { get; set; }
+            public int Argomento1 { get => argomento1; }
+            public int Argomento2 { get => argomento2; }
+
+            public MyClass(int argomento1, int argomento2)
+            {
+               this.argomento1 = argomento1;
+               this.argomento2 = argomento2;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var list = new List<MyClass>()
+            {
+                new MyClass(1,2),
+                new MyClass(3,4),
+                new MyClass(5,6),
+                new MyClass(7,8)
+            };
+            foreach (var elem in list.MyExtensionMethod(true))
+            {
+                Console.WriteLine(elem);
+            }
+        }
+
+        public static IEnumerable<int> MyExtensionMethod(this IEnumerable<IMyinterface> seq, bool pred)
+        {
+            if (seq == null)
+                throw new ArgumentNullException();
+            foreach (var elem in seq)
+            {
+                if (pred)
+                    yield return elem.Argomento1;
+                else yield return elem.Argomento2;
+            }
+        }
+    }
+}
---
+++ test_mio_per_Mock/test_mio_per_Mock_test/Class1.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Linq;
+using test_mio_per_Mock;
+
+namespace test_mio_per_Mock_test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var MockList = new List<Mock<test_mio_per_Mock.Class1.IMyinterface>>();
+            for(int i=0;i<10;i++)
+            {
+                var mockElem = new Mock<test_mio_per_Mock.Class1.IMyinterface>();
+                mockElem.Setup(mb => mb.Argomento1).Returns(i);
+                mockElem.Setup(mb => mb.Argomento2).Returns(i + 1);
+                MockList.Add(mockElem);
+            }
+
+            MockList.Select(s => s.Object).MyExtensionMethod(true).ToList();
+
+            foreach (var item in MockList)
+            {
+                item.Verify(i=>i.Argomento1,Times.AtLeastOnce);
+            }
+
+            MockList.Select(s => s.Object).MyExtensionMethod(false).ToList();
+
+            foreach (var item in MockList)
+            {
+                item.Verify(i => i.Argomento2, Times.AtLeastOnce);
+            }
+        }
+    }
+}
